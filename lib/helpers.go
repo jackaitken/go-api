4add6c66393ea6a5f71fe6fdb4117a735867b706
Jack <john.aitken@aexp.com>
2022-11-19 11:22:05 -0500

    Some refactoring and additions

--- lib/helpers.go
+++ lib/helpers.go
@@ -7,7 +7,7 @@ import (
 	"os"
 )
 
-func LoadJson() ([]TodoList, error) {
+func LoadJSON() ([]TodoList, error) {
 	jsonFile, err := os.ReadFile("sample/sample_data.json")
 
 	if err != nil {
@@ -25,10 +25,8 @@ func LoadJson() ([]TodoList, error) {
 	return todoLists, nil
 }
 
-func AppendTodoList(newList TodoList, existingLists []TodoList) error {
-	existingLists = append(existingLists, newList)
-
-	result, err := json.Marshal(existingLists)
+func SaveJSON(todoLists []TodoList) error {
+	result, err := json.Marshal(todoLists)
 
 	if err != nil {
 		return err
@@ -43,8 +41,63 @@ func AppendTodoList(newList TodoList, existingLists []TodoList) error {
 	return nil
 }
 
+func AppendTodoList(newList TodoList) error {
+	existingLists, err := LoadJSON()
+
+	if err != nil {
+		return err
+	}
+
+	existingLists = append(existingLists, newList)
+
+	err = SaveJSON(existingLists)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func AppendTodo(todo Todo, todoListId int) error {
+	existingLists, err := LoadJSON()
+
+	if err != nil {
+		panic(err)
+	}
+
+	for i := range existingLists {
+		if existingLists[i].Id == todoListId {
+			list := &existingLists[i]
+			list.Todos = append(list.Todos, todo)
+
+			if err := SaveJSON(existingLists); err != nil {
+				panic(err)
+			}
+			return nil
+		}
+	}
+
+	return errors.New("no list with that id was found")
+}
+
+func GetTodoList(id int) (TodoList, error) {
+	todoLists, err := LoadJSON()
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, list := range todoLists {
+		if list.Id == id {
+			return list, nil
+		}
+	}
+	return TodoList{}, errors.New("Could not find list with that id")
+}
+
 func GetTodo(id int) (Todo, error) {
-	todoLists, err := LoadJson()
+	todoLists, err := LoadJSON()
 
 	if err != nil {
 		panic(err)
